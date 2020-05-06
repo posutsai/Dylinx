88b6488e5523ec4b46778fe09fdfadf1e042eb0b
posutsai <posutsai@gmail.com>
2020-05-06 19:53:38 +0800

    Avoid language inconsistency and refine the glue code

--- lock-insertion/src/glue/glue.h
+++ lock-insertion/src/glue/glue.h
@@ -16,82 +16,62 @@
 static_assert(0, "LOCKTYPE_CNT is one of required macro");
 #endif
 
-#define PROB_SELECTION_RESOLUTION 10000
 
-void __dylinx_init__() {
-  srand((uint32_t) time(0));
+enum lock_type {
+  Ttas, Mutex, Ttas_ls, Spinlock
+};
+
+typedef struct AbstractLock {
+  enum lock_type;
+  int32_t id;
+  // the value here can only be assigned in initializer and it
+  // is NULL while contructing.
+  void *block;
+} AbstractLock_t;
+
+typedef int (*initializer_fn)(void *, pthread_mutexattr_t *);
+static initializer_fn initializers[] {
+  ttas_init,
+  mutex_init,
+  ttas_ls_init,
+  spinlock_init
+};
+
+typedef int (*locker_fn)(void *, int);
+static locker_fn lockers[] {
+  ttas_lock,
+  mutex_lock,
+  ttas_ls_lock,
+  spinlock_lock
+};
+
+typedef int (*unlocker_fn)(void *, int);
+static locker_fn unlockers[] {
+  ttas_unlock,
+  mutex_unlock,
+  ttas_ls_unlock,
+  spinlock_unlock
+};
+
+typedef int (*destroyer_fn)(void *);
+static destroyer_fn destroyers[] {
+  ttas_destroy,
+  mutex_destroy,
+  ttas_ls_destroy,
+  spinlock_destroy
+};
+
+int __dylinx_slot_init(AbstractLock_t *ablock, const pthread_mutexattr_t *attr) {
+  return initializers[ablock->lock_type](ablock->block, attr);
 }
-
-template <typename T>
-BaseLock *create_lock() {
-  return new T();
+int __dylinx_slot_lock(AbstractLock_t *ablock, int lock_id) {
+  return lockers[ablock->lock_type](ablock->block, lock_id);
 }
-
-typedef BaseLock *(*create_fn)();
-create_fn creators[LOCKTYPE_CNT] = {
-  // &create_lock<MutexLock>,
-  &create_lock<TTASLock>,
-  &create_lock<TTASLock>,
-  &create_lock<TTASLock>
-};
-enum class LockType {
-  TTAS,
-  Mutex
-};
-class AbstractLock {
-public:
-  AbstractLock(const float *prob_dist=NULL) {
-    uint32_t divider[LOCKTYPE_CNT];
-    sp_lock = nullptr;
-    float prob_sum = 0.;
-    for (uint32_t l = 0; l < LOCKTYPE_CNT; l++) {
-      prob_sum += prob_dist[l];
-      divider[l] = PROB_SELECTION_RESOLUTION * prob_sum;
-    }
-    uint32_t s = rand() % PROB_SELECTION_RESOLUTION;
-    for (uint32_t t = 0;; t++) {
-      if (t == LOCKTYPE_CNT - 1)
-        break;
-      if (s <= divider[t])
-        sp_lock = creators[t]();
-    }
-  }
-  int forward_init(const pthread_mutexattr_t *attr) {
-    return sp_lock->init(attr);
-  }
-  int forward_lock() {
-    return sp_lock->lock();
-  }
-  int forward_unlock() {
-    return sp_lock->unlock();
-  }
-  int forward_destroy() {
-    return sp_lock->destroy();
-  }
-  ~AbstractLock() {
-    delete sp_lock;
-  }
-private:
-  BaseLock *sp_lock;
-};
-
-
-// class MutexLock: public BaseLock {
-// public:
-//   int init(const pthread_mutexattr_t *attr) {
-//     printf("MutexLock instance is initialized!!!!\n");
-//     return pthread_mutex_init(&mutex, attr);
-//   }
-//   int lock() { return pthread_mutex_lock(&mutex); }
-//   int unlock() { return ptread_mutex_unlock(&mutex); }
-//   int destroy() { return pthread_mutex_destroy(&mutex); }
-// private:
-//   pthread_mutex_t mutex;
-// };
-
-int __dylinx_slot_init__(AbstractLock *lock, const pthread_mutexattr_t *attr) { return lock->forward_init(attr); }
-int __dylinx_slot_lock__(AbstractLock *lock) { return lock->forward_lock(); }
-int __dylinx_slot_unlock__(AbstractLock *lock) { return lock->forward_unlock(); }
-int __dylinx_slot_destroy__(AbstractLock *lock) { return lock->forward_destroy(); }
+int __dylinx_slot_unlock(AbstractLock *ablock, int lock_id) {
+  return unlockers[ablock->lock_type](ablock->block, lock_id);
+}
+int __dylinx_slot_destroy(AbstractLock *ablock) {
+  return destroyers[ablock->lock_type](ablock->block);
+}
 
 #endif
