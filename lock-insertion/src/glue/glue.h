75958bcbac21cf4c2f22864a185d48b49efac452
posutsai <posutsai@gmail.com>
2020-05-21 01:24:36 +0800

    Finish indirect pthread function call and fix bug memcpy

--- lock-insertion/src/glue/glue.h
+++ lock-insertion/src/glue/glue.h
@@ -29,19 +29,35 @@ struct Methods4Lock {
 };
 
 #define DYLINX_PTHREADMTX_ID 0
+int indirect_pthread_mutex_init(void *mtx, pthread_mutexattr_t *attr) {
+  return pthread_mutex_init((pthread_mutex_t *)mtx, NULL);
+}
+
+int indirect_pthread_mutex_lock(void *mtx) {
+  return pthread_mutex_lock((pthread_mutex_t *)mtx);
+}
+
+int indirect_pthread_mutex_unlock(void *mtx) {
+  return pthread_mutex_unlock((pthread_mutex_t *)mtx);
+}
+
+int indirect_pthread_mutex_destroy(void *mtx) {
+  return pthread_mutex_destroy((pthread_mutex_t *)mtx);
+}
+
 
 static struct Methods4Lock id2methods_table[LOCK_TYPE_CNT] = {
   {
-    pthread_mutex_init,
-    pthread_mutex_lock,
-    pthread_mutex_unlock,
-    pthread_mutex_destroy
+    indirect_pthread_mutex_init,
+    indirect_pthread_mutex_lock,
+    indirect_pthread_mutex_unlock,
+    indirect_pthread_mutex_destroy
   }
 };
 
 int dylinx_retrievetype_init(entity_with_type_t ewt, pthread_mutexattr_t *attr) {
   struct Methods4Lock m;
-  memcpy(&id2methods_table[ewt.mtx_type], &m, sizeof(struct Methods4Lock));
+  memcpy(&m, &id2methods_table[ewt.mtx_type], sizeof(struct Methods4Lock));
   generic_interface_t *interface = (generic_interface_t *)ewt.entity;
   interface->lock_init = m.initializer;
   interface->lock_enable = m.locker;
