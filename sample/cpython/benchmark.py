9dae44a3bbe0ed51611c43433ad987b23ce09dd5
posutsai <posutsai@gmail.com>
2020-11-07 16:56:58 +0800

    Complete cpython search automation preliminarily
    1. Finish generating perf_log in same mode.

--- sample/cpython/benchmark.py
+++ sample/cpython/benchmark.py
@@ -3,33 +3,80 @@ import sys
 import argparse
 import os
 import subprocess
-from subprocess import PIPE
+import re
 
 if f"{os.environ['DYLINX_HOME']}/python" not in sys.path:
     sys.path.append(f"{os.environ['DYLINX_HOME']}/python")
 from Dylinx import BaseSubject, ALLOWED_LOCK_TYPE
 
+def parse_comparison(std_file, run_file):
+    ptn = re.compile(r"### (.*) ###\n.*\n(Not significant|Significant \(t=([\d.-]*)\))")
+    result = {}
+    with subprocess.Popen(
+        f"pyperformance compare perf_log/{std_file} perf_log/{run_file}".split(' '),
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    ) as proc:
+        stdout = proc.stdout.read().decode("utf-8")
+        record = parse(stdout)
+        if len(record.keys()) == 0:
+            raise ValueError
+        for m in re.finditer(ptn, stdout):
+            result[m.group(1)] = {
+                "ratio": float(m.group(2)),
+                "slower/faster": m.group(3),
+                "significance": not m.group(4) == "Not significant"
+            }
+    return result
+
 class DylinxSubject(BaseSubject):
     def __init__(self, cc_path):
+        self.bm_list = [
+            "json_dumps",
+            "nbody",
+            "pickle_list",
+            "regex_v8",
+            "scimark"
+        ]
+        self.repo = os.path.abspath(os.path.dirname(cc_path))
+        self.sample_dir = os.path.abspath(os.path.dirname('.'))
+        self.assure_standard()
         super().__init__(cc_path)
-        self.repo = os.path.abspath(os.path.dirname(self.cc_path))
+
+    def assure_standard(self):
+        if not os.path.isfile("./perf_log/native_pthreadmtx.json"):
+            os.chdir(self.repo)
+            proc = subprocess.Popen(
+                f"make; pyperformance run --python=./python -b {','.join(self.bm_list)} -o ../perf_log/native_pthreadmtx.json; /bin/rm -rf venv",
+                shell=True, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL
+            )
+            proc.wait()
+            os.chdir("../")
+        print("Standard performance checked")
 
     def build_repo(self, id2type):
         super().configure_type(id2type)
-        cmd = f"cd repo; make -f dylinx.mk python3-dlx"
+        cmd = f"cd repo; make -f dylinx.mk python3-dlx; cd .."
         proc = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
         err_msg = proc.stderr.read().decode("utf-8")
         if len(err_msg) != 0:
             print(err_msg)
 
-    def execute_repo(self):
+    def execute_repo(self, record_json):
         os.environ["LD_LIBRARY_PATH"] = ":".join([
             f"{self.repo}/.dylinx/lib",
             f"{os.environ['DYLINX_HOME']}/build/lib"
         ])
-        with subprocess.Popen(f"pyperformance run --python=python3-dlx -o py-dlx.json".split(' '), stdout=PIPE, stderr=PIPE) as task:
-            print(task.stdout.read().decode("utf-8"))
-            print(task.stderr.read().decode("utf-8"))
+        if os.path.isfile(record_json):
+            os.remove(record_json)
+        os.chdir(self.repo)
+        with subprocess.Popen(
+            f"pyperformance run --python=./python3-dlx --inherit-environ=LD_LIBRARY_PATH -b {','.join(self.bm_list)} -o {record_json}; /bin/rm -rf venv",
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
+        ) as task:
+            stdout = task.stdout.read().decode("utf-8")
+            stderr = task.stderr.read().decode("utf-8")
+        os.chdir("../")
+        return stdout, stderr
 
     def stop_repo(self):
         return
@@ -37,11 +84,12 @@ class DylinxSubject(BaseSubject):
 def run(args):
     subject = DylinxSubject("./repo/compiler_commands.json")
     sites = subject.get_pluggable()
-    print(sites)
     if args.mode == "same":
         id2type = { s: args.ltype for s in sites.keys() }
-        print(id2type)
         subject.build_repo(id2type)
+        stdout, stderr = subject.execute_repo(f"{subject.sample_dir}/perf_log/same/{args.ltype.lower()}.json")
+        print(stdout)
+        print(stderr)
 
 
 if __name__ == "__main__":
