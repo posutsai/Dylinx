74971230a6109c7f764db936fd312b4acd6766c0
posutsai <posutsai@gmail.com>
2020-12-24 12:26:08 +0800

    Use pthread_(set|get)specific to store and retrieve second argument in mcs.

--- src/glue/lock/mcs-lock.h
+++ src/glue/lock/mcs-lock.h
@@ -1,4 +1,9 @@
 #include "dylinx-utils.h"
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/syscall.h>
 #ifndef __DYLINX_MCS_LOCK__
 #define __DYLINX_MCS_LOCK__
 typedef struct mcs_node {
@@ -6,38 +11,38 @@ typedef struct mcs_node {
   volatile int spin  __attribute__((aligned(L_CACHE_LINE_SIZE)));
 } mcs_node_t __attribute__((aligned(L_CACHE_LINE_SIZE)));
 
-typedef struct {
-  mcs_node_t *volatile tail __attribute__((aligned(L_CACHE_LINE_SIZE)));
-  mcs_node_t *volatile head __attribute__((aligned(L_CACHE_LINE_SIZE)));
-} ht_pair_t;
-
 typedef struct mcs_lock {
   pthread_mutex_t posix_lock;
-  ht_pair_t ht;
+  pthread_key_t key;
+  mcs_node_t *volatile tail __attribute__((aligned(L_CACHE_LINE_SIZE)));
 } mcs_lock_t __attribute__((aligned(L_CACHE_LINE_SIZE)));
 
 int mcs_init(void **entity, pthread_mutexattr_t *attr) {
   *entity = (mcs_lock_t *)alloc_cache_align(sizeof(mcs_lock_t));
   mcs_lock_t *mtx = *entity;
-  mtx->ht.tail = NULL;
+  pthread_key_create(&mtx->key, NULL);
+  mtx->tail = NULL;
   return pthread_mutex_init_original(&mtx->posix_lock, attr);
 }
 
 int __mcs_lock(mcs_lock_t *mtx) {
   mcs_node_t *node = (mcs_lock_t *)alloc_cache_align(sizeof(mcs_node_t));
+  printf("mtx addr is %p\n", mtx);
+  printf("locking node is %p\n", node);
+  if (pthread_setspecific(mtx->key, node)) {
+    printf("error msg: %s\n", strerror(errno));
+    exit(0);
+  }
   node->next = NULL;
   node->spin = LOCKED;
-  mcs_node_t *prev_tail = xchg_64((void *)&mtx->ht.tail, (void *)node);
-  if (!prev_tail) {
-    mtx->ht.head = node;
+  mcs_node_t *tail = xchg_64((void *)&mtx->tail, (void *)node);
+  if (!tail)
     return 0;
-  }
-  prev_tail->next = node;
+  tail->next = node;
   COMPILER_BARRIER();
-  // while (__sync_val_compare_and_swap(&node->is_locked, UNLOCKED, LOCKED) == LOCKED)
-  //   CPU_PAUSE();
   while (node->spin == LOCKED)
     CPU_PAUSE();
+  printf("[tid = %ld] running head is %p\n", syscall(__NR_gettid), node);
   return 0;
 }
 
@@ -50,42 +55,40 @@ int mcs_lock(void *entity) {
 
 int mcs_trylock(void *entity) {
   mcs_lock_t *mtx = entity;
-  mcs_node_t *prev_tail;
   mcs_node_t *node = (mcs_lock_t *)alloc_cache_align(sizeof(mcs_node_t));
   node->next = NULL;
   node->spin = LOCKED;
-  mcs_node_t empty = {NULL, NULL};
-  __atomic_exchange(&mtx->ht, &empty, prev_tail, __ATOMIC_SEQ_CST);
-  if (!mtx->ht.tail) {
+  pthread_setspecific(mtx->key, &node);
+  __atomic_compare_exchange(&mtx->tail, NULL, node, 0, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
+  if (!mtx->tail) {
     int ret = 0;
     while ((ret = pthread_mutex_trylock_original(&mtx->posix_lock)) == EBUSY);
     assert(ret == 0);
+    return 0;
   }
   return EBUSY;
 }
 
 int __mcs_unlock(mcs_lock_t *mtx) {
-  mcs_node_t *running = mtx->ht.head;
-  // No visible successor but possible to wait for
-  // future successor.
-  if (!running->next) {
-    ht_pair_t ref = {running, running};
-    ht_pair_t empty = {NULL, NULL};
-    if (__atomic_compare_exchange(&mtx->ht, &ref, &empty, 0, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST))
+  printf("mtx addr is %p\n", mtx);
+  mcs_node_t *node = (mcs_node_t *)pthread_getspecific(mtx->key);
+  printf("unlocking node is %p\n", node);
+  mcs_node_t *empty = NULL;
+  if (!node->next) {
+    if (__atomic_compare_exchange(&mtx->tail, &node, &empty, 0, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST))
       return 0;
-    while (!running->next)
+    while (!node->next)
       CPU_PAUSE();
   }
-  mtx->ht.head = running->next;
-  running->next->spin = UNLOCKED;
-  free(running);
+  node->next->spin = UNLOCKED;
+  printf("[tid = %ld] perform unlock spinning node addr is %p\n", syscall(__NR_gettid), node);
   return 0;
 }
 
 int mcs_unlock(void *entity) {
   mcs_lock_t *mtx = entity;
   pthread_mutex_unlock_original(&mtx->posix_lock);
-  __mcs_lock(mtx);
+  return __mcs_unlock(mtx);
 }
 
 int mcs_destroy(void *entity) {
