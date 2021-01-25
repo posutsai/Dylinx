dabe43244e349a99adc361233307c37e640d3a1c
posutsai <posutsai@gmail.com>
2021-01-25 16:27:20 +0800

    Refine script and plot scalability of optimal combination

--- sample/pipe/analyze.py
+++ sample/pipe/analyze.py
@@ -7,9 +7,13 @@ import re
 import itertools
 import pickle
 import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+from multiprocessing import cpu_count
 if f"{os.environ['DYLINX_HOME']}/python" not in sys.path:
     sys.path.append(f"{os.environ['DYLINX_HOME']}/python")
 from Dylinx import BaseSubject, DylinxRuntimeReport, ALLOWED_LOCK_TYPE
+from glob import glob
 
 class PipeSubject(BaseSubject):
     def __init__(self, cc_path):
@@ -24,13 +28,13 @@ class PipeSubject(BaseSubject):
         self.revert_repo()
         pass
 
-    def build_repo(self, id2type):
+    def build_repo(self, id2type, n_thread=cpu_count()):
         super().configure_type(id2type)
         os.environ["LD_LIBRARY_PATH"] = ":".join([
             f"{os.environ['DYLINX_HOME']}/sample/pipe/.dylinx/lib",
             f"{os.environ['DYLINX_HOME']}/build/lib"
         ])
-        cmd = f"make clean; make queue with_dlx=1"
+        cmd = f"make clean; make queue n_thread={n_thread} with_dlx=1"
         proc = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
         log_msg = proc.stdout.read().decode("utf-8")
         err_msg = proc.stderr.read().decode("utf-8")
@@ -74,10 +78,45 @@ class PipeSubject(BaseSubject):
     def stop_repo(self):
         return
 
+def measure_scalability():
+    op_per_tu = {}
+    for t in [1, 2, 4, 8, 16, 32, 64]:
+        cmd = f"make queue with_dlx=0 n_thread={t}; ./bin/queue"
+        proc = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+        log_msg = proc.stdout.read().decode("utf-8")
+        err_msg = proc.stderr.read().decode("utf-8")
+        pattern = re.compile(r"Duration is (\d+)")
+        entries = re.findall(pattern, log_msg)
+        duration = int(entries[0]) / 1000
+        op_per_tu[t] = (64 * 128 * 1024 + 64 * 128) / duration
+    return op_per_tu
 
-if __name__ == "__main__":
-    REPEAT_EXP = 5
-    with PipeSubject("./compile_commands.json") as subject:
+def plot_scalability(op_per_tu, fig_path):
+    threads = sorted(op_per_tu.keys())
+    measure = [op_per_tu[x] for x in threads]
+    linear = [measure[0] * x for x in threads]
+    fig, ax = plt.subplots()
+    ax.set_title("Performance scalability in MPMC queue")
+    ax.plot(threads, measure, label="actual", marker="o")
+    ax.plot(threads, linear, label="linear", marker="o")
+    plt.yscale("log")
+    plt.xlabel("thread number")
+    plt.ylabel(f"op per ms")
+    ax.legend()
+    plt.savefig(fig_path)
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+def brute_force(cc_path, pkl_path):
+    with PipeSubject(cc_path) as subject:
         sites = subject.get_pluggable()
         sorted_site_ids = sorted(sites.keys())
         combs = list(itertools.product(*([ALLOWED_LOCK_TYPE] * len(sorted_site_ids))))
@@ -91,6 +130,97 @@ if __name__ == "__main__":
                 durations.append(dur)
             logs[comb] = (np.mean(durations), np.std(durations))
             print(f"{comb} mean: {logs[comb][0]:.2e}, std: {logs[comb][1]:.2e}")
-        with open("logs.pkl", "wb") as handler:
+        with open(pkl_path, "wb") as handler:
             pickle.dump(logs, handler)
+def inspect_scalability(combs, cc_path, pkl_path, fig_path):
+    with PipeSubject(cc_path) as subject:
+        sites = subject.get_pluggable()
+        sorted_site_ids = sorted(sites.keys())
+        logs = {}
+        n_threads = [1, 2, 4, 8, 16, 32, 64]
+        for comb in combs:
+            id2type = {i: t for i, t in enumerate(comb)}
+            subject.build_repo(id2type)
+            op_per_tu = {}
+            for t in n_threads:
+                subject.build_repo(id2type, t)
+                durations = []
+                for i in range(3):
+                    durations.append(subject.execute_native() / 1000)
+                op_per_tu[t] = (64 * 128 * 1024 + 64 * 128) / np.mean(durations)
+            logs[comb] = op_per_tu
+            with open(pkl_path, "wb") as handler:
+                pickle.dump(logs, handler)
+            fig, ax = plt.subplots()
+            ax.set_title("Performance scalability among combinations in MPMC queue")
+            for k in logs.keys():
+                measure = [logs[k][t] for t in n_threads]
+                ax.plot(n_threads, measure, label=f"{k}", marker="o")
+            plt.xlabel("thread number")
+            plt.ylabel(f"op per ms")
+            ax.legend()
+            plt.grid()
+            plt.savefig(fig_path)
+            print(f"{comb} completes")
 
+def get_latest_id(file_ptn, re_ptn):
+    existing_logs = sorted(glob(file_ptn))
+    ptn = re.compile(re_ptn)
+    log_ids = []
+    for l in existing_logs:
+        log_ids.append(int(ptn.findall(os.path.basename(l))[0]))
+    save_id = 0 if len(log_ids) == 0 else max(log_ids)
+    return save_id
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--plot_scalable",
+        "-ps",
+        type=str2bool,
+        required=False,
+        default=False,
+    )
+    parser.add_argument(
+        "--brute_force",
+        "-bf",
+        type=str2bool,
+        required=False,
+        default=False,
+    )
+    parser.add_argument(
+        "--comb_scalable",
+        "-cs",
+        type=str2bool,
+        required=False,
+        default=False,
+    )
+    args = parser.parse_args()
+    REPEAT_EXP = 5
+    if args.plot_scalable == True:
+        op_per_tu = measure_scalability()
+        plot_scalability(op_per_tu, "mpmc_scalability.png")
+
+    elif args.brute_force == True:
+        save_id = get_latest_id(
+            "./save/exec_time/execution-*.pkl",
+            r"execution-(\d)+.pkl"
+        )
+        brute_force(
+            "./compile_commands.json",
+            f"./save/comb2exetime-{save_id}.pkl"
+        )
+
+    elif args.comb_scalable == True:
+        save_id = get_latest_id(
+            "./save/scalability/scalability-*.pkl",
+            r"scalability-(\d)+.pkl"
+        )
+        inspect_scalability(
+            [ ("PTHREADMTX", "PTHREADMTX"), ("BACKOFF", "BACKOFF"), ("ADAPTIVEMTX", "BACKOFF")],
+            "./compile_commands.json",
+            f"./save/scalability/scalability-{save_id}.pkl",
+            "mpmc_comb_scale.png"
+        )
+    else:
+        print("Nothing has been done")
--- sample/pipe/main.c
+++ sample/pipe/main.c
@@ -3,15 +3,17 @@
 #include <pthread.h>
 #include <x86intrin.h>
 #include <unistd.h>
+#include <sys/time.h>
 #include "pipe.h"
-#define N_THREAD 64
 #define BULK_SIZE 1024
 #define REPEAT 128
+#define TOTAL_PUSH_OP 64 * REPEAT * BULK_SIZE / N_THREAD
+#define TORAL_POP_OP 64 * REPEAT / N_THREAD
 
 void *producer_op(void *arg) {
     pipe_producer_t *handler = (pipe_producer_t *)arg;
     int buff = 1;
-    for (int t = 0; t < BULK_SIZE * REPEAT; t++) {
+    for (int t = 0; t < TOTAL_PUSH_OP; t++) {
         pipe_push(handler, &buff, sizeof(int));
     }
     return NULL;
@@ -20,7 +22,7 @@ void *producer_op(void *arg) {
 void *consumer_op(void *arg) {
     pipe_consumer_t *handler = (pipe_consumer_t *)arg;
     int buff[BULK_SIZE];
-    for (int t = 0; t < REPEAT; t++)
+    for (int t = 0; t < TORAL_POP_OP; t++)
         pipe_pop(handler, buff, BULK_SIZE * sizeof(int));
     return NULL;
 }
@@ -31,7 +33,8 @@ int main(int argc, char *argv[]) {
     pipe_t *q = pipe_new(1, 0);
     pipe_producer_t *pushers[N_THREAD];
     pipe_consumer_t *poppers[N_THREAD];
-    uint64_t timer_s = __rdtsc();
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
     for (int i = 0; i < N_THREAD; i++) {
         pushers[i] = pipe_producer_new(q);
     }
@@ -46,7 +49,8 @@ int main(int argc, char *argv[]) {
         pthread_join(producers[i], NULL);
         pthread_join(consumers[i], NULL);
     }
-    uint64_t timer_e = __rdtsc();
-    printf("Duration is %lu\n", timer_e - timer_s);
+    clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+    uint64_t delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
+    printf("Duration is %lu\n", delta_us);
 	return 0;
 }
